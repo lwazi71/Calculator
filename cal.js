1544486d1a648a8dbeb4ddb9f3e562f7a61baa96
Lwazi Mabota <greninja71@gmail.com>
2021-12-27 12:11:34 -0800

    Added html bolierplate and basic math functions for calculator

---
+++ cal.js
@@ -0,0 +1,24 @@
+const add = function(a,b) {
+    return a + b;
+};
+const subtract = function(a,b) {
+    return a-b;
+};
+const multiply = function(a,b) {
+    return a * b;
+}
+const divide = function(a,b) {
+    return a/b;
+}
+function operate(num1,num2,operator) {
+    switch(operator){
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+    }
+}
