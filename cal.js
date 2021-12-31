218aa75ddda52f3161e3e33ccea8749993b63269
Lwazi Mabota <greninja71@gmail.com>
2021-12-30 17:54:32 -0800

    Fixed html ID names and allowed for calculator to operate based on uer click

--- cal.js
+++ cal.js
@@ -1,14 +1,15 @@
 const numberButton = document.querySelectorAll('.number');
 const operatorButton = document.querySelectorAll('.operator');
-const clearButton = document.querySelector('.clear');
+const clearButton = document.getElementById('#clear');
 const currentOperand = document.querySelector('.currentOperator');
 const previousOperand = document.querySelector('.previousOperator');
-const equalButton = document.querySelector('.equals-key');
-const showResult = document.querySelector('.result');
-let display = '0';
-let num1 = '';
-let num2 ='';
-let operator = '';
+const equalButton = document.getElementById('#equal');
+const divideButton = document.getElementById('#divide');
+const multiplyButton = document.getElementById('#multiply');
+const addButton = document.getElementById('#add');
+const subtractButton = document.getElementById('#subtract');
+currentOperand.textContent = '';
+previousOperand.textContent = '';
 
 const displayValue = function (e) {
     const value = e.target.value;
@@ -19,7 +20,24 @@ const displayValue = function (e) {
  allBtns.forEach( element => {
      element.addEventListener('click', displayValue)
  })
- 
+/* numberButton.forEach(numberButton => {
+    numberButton.addEventListener('click', e => {
+        if(operator === '') {
+            num1 += e.target.innerText;
+            console.log(num1);
+        } else{
+            num2 += e.target.innrText;
+            console.log(num2)
+        }
+    });
+});
+operatorButton.forEach(operatorButton => {
+    operatorButton.addEventListener('click',e => {
+       operatorButton = e.target.innerText;
+       console.log(operatorButton)
+    });
+});
+*/
 const add = function(a,b) {
     return a + b;
 };
@@ -44,20 +62,38 @@ function operate(num1,num2,operator) {
             return divide(num1,num2);
     }
 }
-numberButton.forEach(numberButton => {
-    numberButton.addEventListener('click', e => {
-        if(operator === '') {
-            num1 += e.target.innerText;
-            console.log(num1);
-        } else{
-            num2 += e.target.innrText;
-            console.log(num2)
-        }
-    });
+let storedNumber = '';
+let clickedOperator =''
+let firstNum = '';
+let result = '';
+currentOperand.textContent = 0;
+
+numberButton.forEach((number) => {
+    number.addEventListener('click', function() {
+        storedNumber += number.value;
+        currentOperand.textContent = storedNumber;
+    })
 });
-operatorButton.forEach(operatorButton => {
-    operatorButton.addEventListener('click',e => {
-       operatorButton = e.target.innerText;
-       console.log(operatorButton)
-    });
-});
+operatorButton.forEach((operator => {
+    operator.addEventListener('click', function() {
+       if(firstNum && storedNumber){
+        displayResult()
+       }
+       firstNum = storedNumber;
+       clickedOperator = operator.textContent;
+       previousOperand.textContent = storedNumber + clickedOperator;
+       storedNumber ='';
+       console.log('FirstNumber' + firstNum + 'Stored' + storedNumber)
+       console.log(clickedOperator);
+    })
+}));
+
+equalButton.addEventListener('click', function() {
+    displayResult()
+})
+function displayResult() {
+    result = operate(parseFloat(firstNum), parseFloat(storedNumber), clickedOperator)
+    currentOperand.textContent = result;
+    previousOperand.textContent = firstNum + ' ' + clickedOperator + ' ' + storedNumber;
+    storedNumber = result;
+}
