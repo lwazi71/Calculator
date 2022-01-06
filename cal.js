30db1aa6a7f9c3e774c847759bc83edcef4f802d
Lwazi Mabota <greninja71@gmail.com>
2022-01-06 10:56:30 -0800

    Added backspace function

--- cal.js
+++ cal.js
@@ -1,13 +1,9 @@
+/** DOM Elements */
 const numberButton = document.querySelectorAll('.number');
 const operatorButton = document.querySelectorAll('.operator');
 const clearButton = document.getElementById('CE');
 const deleteButton = document.getElementById('C');
 const percentButton = document.getElementById('percent');
-const changeSign = document.getElementById('negpositive');
-/*
-const currentOperand = document.querySelector('.currentOperator');
-const previousOperand = document.querySelector('.previousOperator');
-*/
 const equalButton = document.getElementById('equal');
 const divideButton = document.getElementById('divide');
 const multiplyButton = document.getElementById('multiply');
@@ -15,48 +11,47 @@ const addButton = document.getElementById('add');
 const subtractButton = document.getElementById('subtract');
 const display = document.querySelectorAll('.display');
 const show = document.querySelector("#show");
-
-/*
-currentOperand.textContent = '';
-previousOperand.textContent = '';
-*/
+/** State Variables */
+let storedNumber = '';
+let clickedOperator ='';
+let firstNum = '';
+let result = '';
+/** Display Functions */
 const displayValue = function (e) {
     const value = e.target.value;
     document.getElementById('show').value += value;
  }
-
- const numberButtons = document.querySelectorAll('.number');
- numberButtons.forEach (elemnet => {
-     elemnet.addEventListener('click',displayValue);
- })
-
-
- const allBtns = document.querySelectorAll('.myBtn');
- allBtns.forEach( element => {
-     element.addEventListener('click', displayValue)
- })
- const backspaceButton = document.querySelector('.backspace')
- backspaceButton.addEventListener('click',backspace);
-/* numberButton.forEach(numberButton => {
-    numberButton.addEventListener('click', e => {
-        if(operator === '') {
-            num1 += e.target.innerText;
-            console.log(num1);
-        } else{
-            num2 += e.target.innrText;
-            console.log(num2)
-        }
-    });
+ numberButton.forEach((number) => {
+    number.addEventListener('click', function() {
+        storedNumber += number.value;
+        show.textContent = storedNumber;
+    })
 });
-operatorButton.forEach(operatorButton => {
-    operatorButton.addEventListener('click',e => {
-       operatorButton = e.target.innerText;
-       console.log(operatorButton)
-    });
-});
-*/
+operatorButton.forEach((operator => {
+    operator.addEventListener('click', function() {
+       if(firstNum && storedNumber){
+        displayResult()
+    }
+       if(storedNumber) firstNum = storedNumber;
+       clickedOperator = operator.textContent;
+       show.textContent = storedNumber + clickedOperator;
+       storedNumber ='';
+    })
+}));
+function backSpace() {
+    storedNumber = storedNumber.substr(0,storedNumber.length-1);
+    show.textContent = storedNumber;
+    console.log(storedNumber)
+}
+ function displayResult() {
+    result = operate(parseFloat(firstNum), parseFloat(storedNumber), clickedOperator)
+    show.textContent = result;
+    show.textContent = firstNum  + storedNumber;
+    storedNumber = result;
+}
 
-const add = function(a,b) {
+ /**Operation Function */
+ const add = function(a,b) {
     return a + b;
 };
 const subtract = function(a,b) {
@@ -71,14 +66,6 @@ const divide = function(a,b) {
     }
     return a/b;
 }
-/*
-const percent = function(a,b) {
-    return(a/100 * b).toFixed(2)
-}
-const round = function(a,b){
-    return Math.round(a,b*100)/100;
-}
-*/
 function operate(num1,num2,operator) {
     switch(operator){
         case "+":
@@ -90,40 +77,24 @@ function operate(num1,num2,operator) {
         case "/":
             return divide(num1,num2);
         case "=":
-        return num2;
+            return num2;
+            
     }
 }
-let storedNumber = '';
-let clickedOperator =''
-let firstNum = '';
-let result = '';
-/*
-currentOperand.textContent = 0;
-*/
+/** Click Events */
+ const numberButtons = document.querySelectorAll('.number');
+ numberButtons.forEach (elemnet => {
+     elemnet.addEventListener('click',displayValue);
+ })
 
+const allBtns = document.querySelectorAll('.myBtn');
+ allBtns.forEach( element => {
+     element.addEventListener('click', displayValue)
+ })
+const backspaceButton = document.querySelector('.backspace')
+ backspaceButton.addEventListener('click',backSpace);
 
-numberButton.forEach((number) => {
-    number.addEventListener('click', function() {
-        storedNumber += number.value;
-        show.textContent = storedNumber;
-    })
-});
-operatorButton.forEach((operator => {
-    operator.addEventListener('click', function() {
-       if(firstNum && storedNumber){
-        displayResult()
-       }
-       if(storedNumber) firstNum = storedNumber;
-       clickedOperator = operator.textContent;
-       show.textContent = storedNumber + clickedOperator;
-       storedNumber ='';
-       /*
-       console.log('FirstNumber' + firstNum + 'Stored' + storedNumber)
-       console.log(clickedOperator);
-       */
-    })
-}));
-equalButton.addEventListener('click', () => {
+ equalButton.addEventListener('click', () => {
     show.textContent = firstNum;
 })
 percentButton.addEventListener('click', ()=> {
@@ -133,20 +104,11 @@ percentButton.addEventListener('click', ()=> {
 })
 clearButton.addEventListener('click', () => {
     storedNumber ='';
+    firstNum = "";
     document.getElementById('show').innerText ='';
 })
 deleteButton.addEventListener('click',() => {
     storedNumber = '';
+    firstNum = "";
     document.getElementById('show').innerText= 0;
 })
-function backspace() {
-    var value = document.getElementById("show").innerText;
-    document.getElementById("show").innerText= value = value.substr(0,value.length-1);
-}
-
-function displayResult() {
-    result = operate(parseFloat(firstNum), parseFloat(storedNumber), clickedOperator)
-    show.textContent = result;
-    show.textContent = firstNum  + storedNumber;
-    storedNumber = result;
-}
