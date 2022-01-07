1e351d43047e9109b7ff4a0e2e6786c83d129420
Lwazi Mabota <greninja71@gmail.com>
2022-01-07 12:57:30 -0800

    Removed HTML comments and implemented round function for bigger calculations

--- cal.js
+++ cal.js
@@ -38,18 +38,17 @@ operatorButton.forEach((operator => {
        storedNumber ='';
     })
 }));
-function backSpace() {
-    storedNumber = storedNumber.substr(0,storedNumber.length-1);
-    show.textContent = storedNumber;
-    console.log(storedNumber)
-}
  function displayResult() {
     result = operate(parseFloat(firstNum), parseFloat(storedNumber), clickedOperator)
     show.textContent = result;
     show.textContent = firstNum  + storedNumber;
     storedNumber = result;
-}
-
+ }
+ function roundNumber(calculate){
+    var rounded = Math.round(calculate * 1000) / 1000;
+    return rounded;
+ }
+ roundNumber(firstNum);
  /**Operation Function */
  const add = function(a,b) {
     return a + b;
@@ -77,16 +76,19 @@ function operate(num1,num2,operator) {
         case "/":
             return divide(num1,num2);
         case "=":
-            return num2;
-            
+            return num2;     
     }
 }
 /** Click Events */
+function backSpace() {
+    storedNumber = storedNumber.substr(0,storedNumber.length-1);
+    show.textContent = storedNumber;
+    console.log(storedNumber)
+}
  const numberButtons = document.querySelectorAll('.number');
  numberButtons.forEach (elemnet => {
      elemnet.addEventListener('click',displayValue);
  })
-
 const allBtns = document.querySelectorAll('.myBtn');
  allBtns.forEach( element => {
      element.addEventListener('click', displayValue)
@@ -94,12 +96,14 @@ const allBtns = document.querySelectorAll('.myBtn');
 const backspaceButton = document.querySelector('.backspace')
  backspaceButton.addEventListener('click',backSpace);
 
- equalButton.addEventListener('click', () => {
+ equalButton.addEventListener('click', ()=> {
+        var rounded = Math.round(firstNum * 1000) / 1000;
+        firstNum = rounded;
     show.textContent = firstNum;
 })
 percentButton.addEventListener('click', ()=> {
     const curr = parseFloat(firstNum)
-    firstNum= (curr/100) 
+    firstNum = (curr/100);
     console.log(firstNum);
 })
 clearButton.addEventListener('click', () => {
